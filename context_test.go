bc1eb3e77eb61761f3dacda55e7ea1a5e054aab9
Muhamad Sirojudin <muhamad.sirojudin@hyperscal.com>
2024-09-12 11:24:51 +0700

    context get value

---
+++ context_test.go
@@ -0,0 +1,54 @@
+package golangcontext
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+}
+
+func TestGetValueFromContext(t *testing.T) {
+
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextB.Value("c"))
+	fmt.Println(contextC.Value("a"))
+	fmt.Println(contextD.Value("f"))
+	fmt.Println(contextE.Value("b"))
+	fmt.Println(contextF.Value("a"))
+	fmt.Println(contextA.Value("b"))
+}
